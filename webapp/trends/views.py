3b1b0c3b199d04b2318432c76b8d19fe2266f283
Dorofeev_Alexandr <amd_86@mail.ru>
2022-01-24 16:34:21 +0300

    Создан шаблон задания даты

--- webapp/trends/views.py
+++ webapp/trends/views.py
@@ -1,7 +1,22 @@
-from flask import Blueprint, render_template, current_app
-from webapp.trends.models import Trends
-from webapp.weather import weather_by_city
-from datetime import datetime, timedelta, time
+from datetime import datetime
+from flask import Blueprint, render_template
+from webapp.trends.forms import SelectDateForm
+
 
 blueprint = Blueprint('trends', '__name__')
 
+
+@blueprint.route('/monitor', methods=['GET', 'POST'])
+def chart():
+    form = SelectDateForm()
+    if form.select_date.data:
+        select_date = form.select_date.data
+    else:
+        select_date = datetime.today()
+        form.select_date.data = select_date
+    # if form.validate_on_submit():
+    #     s_begin_time = form.select_date.data
+    #     return redirect(url_for('trends.monitor'))
+    title = "Архив температуры"
+    return render_template('trends/index.html',
+                           page_title=title, form=form)
