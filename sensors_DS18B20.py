ec76c8b9b2ddb016c35e6f28f7bf554e1b1dfb3b
Dorofeev_Alexandr <amd_86@mail.ru>
2022-01-29 00:36:38 +0300

    Добавлено обработка отсутствия датчика

--- sensors_DS18B20.py
+++ sensors_DS18B20.py
@@ -10,21 +10,27 @@ class Sensors_DS18B20():
 
     def read_temp_raw(self, sensor_adr):
         # print(device_file)
-        device_folder = glob.glob(self.base_dir + sensor_adr)[0]
-        device_file = device_folder + '/w1_slave'
-        f = open(device_file, 'r')
-        lines = f.readlines()
-        f.close()
-        return lines
+        try:
+            device_folder = glob.glob(self.base_dir + sensor_adr)[0]
+            device_file = device_folder + '/w1_slave'
+            f = open(device_file, 'r')
+            lines = f.readlines()
+            f.close()
+            return lines
+        except(IndexError, TypeError):
+            print('not', sensor_adr)
+            return False
 
     def read_temp(self, sensor_adr):
         lines = self.read_temp_raw(sensor_adr)
         # print(lines)
-        if lines[0].strip()[-3:] == 'YES':
-            equals_pos = lines[1].find('t=')
-            # print(equals_pos)
-            if equals_pos != -1:
-                temp_string = lines[1][equals_pos+2:]
-                temp_c = float(temp_string) / 1000.0
-                return temp_c
+        if lines:
+            if lines[0].strip()[-3:] == 'YES':
+                equals_pos = lines[1].find('t=')
+                # print(equals_pos)
+                if equals_pos != -1:
+                    temp_string = lines[1][equals_pos+2:]
+                    temp_c = float(temp_string) / 1000.0
+                    return temp_c
+            return None
         return None
